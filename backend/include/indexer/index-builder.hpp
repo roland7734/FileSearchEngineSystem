58b5ae048ee0581481005e2f59345b2debf14cbe
roland7734 <149438753+roland7734@users.noreply.github.com>
2025-03-24 01:23:42 +0200

    feat: implement file indexing (index-builder.hpp, index-builder.cpp)
    - Added an indexing mechanism that gathers files from a base directory and inserts them into the database in batches.

---
+++ backend/include/indexer/index-builder.hpp
@@ -0,0 +1,25 @@
+// index-builder.hpp
+
+#ifndef INDEX_BUILDER_HPP
+#define INDEX_BUILDER_HPP
+
+#include "database/database.hpp"
+#include "file-crawler.hpp"
+#include "service/insert-service.hpp"
+
+#include <string>
+#include <vector>
+
+class IndexBuilder {
+public:
+    IndexBuilder(FileCrawler* crawler, InsertService* insertService);
+    void indexTextFiles();
+
+
+private:
+    FileCrawler* crawler;
+    InsertService* insertService;
+
+};
+
+#endif  // INDEX_BUILDER_HPP
---
+++ backend/src/indexer/index-builder.cpp
@@ -0,0 +1,30 @@
+// index-builder.cpp
+
+#include "indexer/index-builder.hpp"
+#include "database/database.hpp"
+#include "indexer/file-crawler.hpp"
+#include "config/config.hpp"
+#include <iostream>
+
+IndexBuilder::IndexBuilder(FileCrawler* crawler, InsertService* insertService)
+        : crawler(crawler), insertService(insertService) {}
+
+void IndexBuilder::indexTextFiles() {
+    std::vector<File> files = crawler->getFilesRecursively();
+    std::vector<File> batch;
+
+    for (const auto& file : files) {
+        batch.push_back(file);
+
+        if (batch.size() == Config::BATCH_SIZE) {
+            insertService->insertBatchToDatabase(batch);
+            batch.clear();
+        }
+    }
+
+    if (!batch.empty()) {
+        insertService->insertBatchToDatabase(batch);
+    }
+
+    std::cout << "Indexing completed for path: " << crawler->getBasePath() << std::endl;
+}
